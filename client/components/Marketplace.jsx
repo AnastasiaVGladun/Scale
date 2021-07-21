718bb84c4349ae007709acd650be9dc932f6a914
Anastasia Gladun <anastasiagladoun@gmail.com>
2021-07-21 17:48:20 +1200

    css

--- client/components/Marketplace.jsx
+++ client/components/Marketplace.jsx
@@ -13,9 +13,8 @@ const Marketplace = (props) => {
   return (
     <div className="marketplace-body">
       <h1 className="marketplace-heading">Marketplace</h1>
-      {/* <div > */}
-        
-      {/* <Link to='/addlisting'><button className='buttonItem'>Add listing</button></Link> */}
+      <div className="listing-container">
+      <div className="add-listing"><Link to='/addlisting'><button>Add listing</button></Link></div>
         {marketplace.map((listing) => {
           return (
           <div className="listing" key={listing.id}>
@@ -27,7 +26,7 @@ const Marketplace = (props) => {
           </div>)
         })}
       </div>
-    // </div>
+    </div>
   )
 }
 

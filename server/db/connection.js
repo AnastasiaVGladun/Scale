6380280460f975413d70fee1bf64d8878fb85a56
Anastasia Gladun <anastasiagladoun@gmail.com>
2022-02-01 12:42:57 +1300

    changed settings for deploying

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,11 @@
-const knex = require('knex')
-const config = require('./knexfile')
-const env = process.env.NODE_ENV || 'development'
-const connection = knex(config[env])
+// const knex = require('knex')
+// const config = require('./knexfile')
+// const env = process.env.NODE_ENV || 'development'
+// const connection = knex(config[env])
 
-module.exports = connection
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const db = require('knex')(config)
+
+module.exports = db
+// module.exports = connection

9cb70496a217c6feeef96a9269234db2e0f583e3
Anastasia Gladun <anastasiagladoun@gmail.com>
2022-02-01 18:02:05 +1300

    change enviroment config

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ if(!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
 }
 
 const server = require('./server')
-const PORT = process.env.PORT || 3000
+// var PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)

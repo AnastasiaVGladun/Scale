7269fd4ae704128756da7f2313c8354a85508c10
AnastasiaVGladun <81542238+AnastasiaVGladun@users.noreply.github.com>
2022-02-01 17:50:50 +1300

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,11 +1,11 @@
-// const env = process.env.NODE_ENV || 'development'
-// const knex = require('knex')
-// const config = require('./knexfile')
-// const connection = knex(config[env])
+ const env = process.env.NODE_ENV || 'development'
+ const knex = require('knex')
+ const config = require('./knexfile')
+ const connection = knex(config[env])
 
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
-const db = require('knex')(configuration)
+// const environment = process.env.NODE_ENV || 'development'
+// const config = require('./knexfile')[environment]
+// const db = require('knex')(configuration)
 
-module.exports = db
-// module.exports = connection
+// module.exports = db
+module.exports = connection

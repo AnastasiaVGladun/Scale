75b92aa37c04f4a4e592f9462d4ec2350ba7489b
Anastasia Gladun <anastasiagladoun@gmail.com>
2022-02-01 16:30:42 +1300

    knex script chenged

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,11 +1,11 @@
-// const knex = require('knex')
-// const config = require('./knexfile')
-// const env = process.env.NODE_ENV || 'development'
-// const connection = knex(config[env])
+const knex = require('knex')
+const config = require('./knexfile')
+const env = process.env.NODE_ENV || 'development'
+const connection = knex(config[env])
 
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
-const db = require('knex')(config)
+// const environment = process.env.NODE_ENV || 'development'
+// const config = require('./knexfile')[environment]
+// const db = require('knex')(configuration)
 
-module.exports = db
-// module.exports = connection
+// module.exports = db
+module.exports = connection
